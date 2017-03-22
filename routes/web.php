44223e228744a6bedd87f8b17c4f8057d1986b92
Adam Pieniazek <adamzp@bu.edu>
2017-03-22 16:23:35 -0400

    Up to episode 6, add tasks table and view

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,35 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+	$tasks = DB::table('tasks')->get();
+
+
+/*	$tasks = [
+		'Go to the store',
+		'Finish screencast',
+		'Clean the house',
+	];
+*/
+	return view('welcome', compact('tasks'));
+
+/* Just a few different ways of passing in data
+
+	$name = "Ground";
+	$age = '34';
+
+	return view('welcome', compact('name', 'age'));
+
+	return view('welcome')->with('name', $name);
+
+    return view('welcome',
+    [
+    	'name' => $name
+    ]
+    );
+*/
+});
+
+Route::get('/about', function() {
+	return view('about');
 });
