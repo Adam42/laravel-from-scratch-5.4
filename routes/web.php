1e7e5d026a528c38a741de644a8487342184f71a
Adam Pieniazek <adamzp@bu.edu>
2017-03-23 10:53:43 -0400

    Move controller code to a controller file

--- routes/web.php
+++ routes/web.php
@@ -11,49 +11,9 @@
 |
 */
 
-use App\Task;
+Route::get('/tasks', 'TasksController@index');
 
-Route::get('/tasks', function () {
-
-#	$tasks = DB::table('tasks')->get();
-
-	$tasks = Task::all();
-
-/*	$tasks = [
-		'Go to the store',
-		'Finish screencast',
-		'Clean the house',
-	];
-*/
-	return view('tasks.index', compact('tasks'));
-
-/* Just a few different ways of passing in data
-
-	$name = "Ground";
-	$age = '34';
-
-	return view('welcome', compact('name', 'age'));
-
-	return view('welcome')->with('name', $name);
-
-    return view('welcome',
-    [
-    	'name' => $name
-    ]
-    );
-*/
-});
-
-Route::get('/tasks/{task}', function($id){
-
-#	$task = DB::table('tasks')->find($id);
-
-	$task = Task::find($id);
-
-	return view('tasks.show', compact('task'));
-
-##	dd($task); //dump and die helper func
-});
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/about', function() {
 	return view('about');
