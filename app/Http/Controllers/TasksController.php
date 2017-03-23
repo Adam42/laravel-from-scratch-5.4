cbd3a8ae738301bc9afc75990d8d783357644bbc
Adam Pieniazek <adamzp@bu.edu>
2017-03-23 11:17:27 -0400

    Using route model binding instead of manually finding model

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -10,14 +10,11 @@ class TasksController extends Controller
     public function index()
     {
         $tasks = Task::all();
-
         return view('tasks.index', compact('tasks'));
     }
 
-    public function show($id)
+    public function show(Task $task)
     {
-        $task = Task::find($id);
-
         return view('tasks.show', compact('task'));
     }
 }
