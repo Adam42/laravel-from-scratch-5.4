e4164b018c03f19d5083f572c5df7f6286eac371
Adam Pieniazek <adamzp@bu.edu>
2017-03-24 16:28:34 -0400

    Adding layouts

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/tasks', 'TasksController@index');
 
 Route::get('/tasks/{task}', 'TasksController@show');
 
+Route::get('/', 'PostsController@index');
+
+Route::get('/posts/{post}', 'PostsController@show');
+
 Route::get('/about', function() {
 	return view('about');
 });
