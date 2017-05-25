3c004c96f98f060b0b4ab3105f4226633c5e3b80
Adam <adam@the42ndestate.com>
2017-05-25 19:26:26 -0400

    Create Comments and associate them with a Post so a Post has many comments and a comment belongsTo a single Post

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    //in tutorial a base Model class is created with an empty guard.
     //in order to use Post::create we need to assign these
     protected $fillable = ['title', 'body'];
 
     //protected $guarded = [] \\this is like the reverse of the above
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
