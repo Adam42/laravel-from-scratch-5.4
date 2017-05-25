8e318a35984e2930c074bd6eaa59b22c08c9f8f0
Adam Pieniazek <adamzp@bu.edu>
2017-05-25 14:20:07 -0400

    Add a form to create and store posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -14,5 +15,35 @@ class PostsController extends Controller
     public function show()
     {
         return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $post = new Post;
+
+        $post->title = request('title');
+        $post->body = request('body');
+
+        $post->save();
+
+        //Or, an alternative way of doing all of the above
+  /* This method will not work by default because of mass assignment
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        OR:
+
+        Post::create(request(['title', 'body']));
+
+*/
+        return redirect('/');
+
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //in order to use Post::create we need to assign these
+    protected $fillable = ['title', 'body'];
+
+    //protected $guarded = [] \\this is like the reverse of the above
 }
